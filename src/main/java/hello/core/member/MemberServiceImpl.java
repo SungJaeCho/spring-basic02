e37aee8522f3f9cca9aa0083af7b5756a2d32851
sungjaecho <liadus@naver.com>
2022-06-09 09:41:15 +0900

    주문추가 및 DIP적용 (순수자바)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     //추상화에도 의존 구현화에도 의존 DIP위반
-    private MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
