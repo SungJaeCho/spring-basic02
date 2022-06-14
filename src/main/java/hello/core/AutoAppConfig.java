624784c994e255cd5b2808ec0dc468fe4b58c56c
sungjaecho <liadus@naver.com>
2022-06-15 08:59:08 +0900

    @ComponentScan  - 기본 스캔대상 : @Component, @Controller, @Service, @Repository, @Configuration
    filter옵션
     - includeFilters : 스캔대상 포함
     - excludeFilters : 스캔대상 제거

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+//        basePackages = "hello.core", // 지정하지 않으면 @ComponentScan이 붙은 설정정보 클래스의 패키지가 시작위치가 됨
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 기존에 등록한 AppConfig.class 제외하기위해 추가
+)
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     //추상화에도 의존 구현화에도 의존 DIP위반
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
