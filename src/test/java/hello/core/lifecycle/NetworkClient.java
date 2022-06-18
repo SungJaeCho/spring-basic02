b74872c6d581846713658f0c8a44949466577124
SungJaeCho <liadus.veil@gmail.com>
2022-06-19 06:28:18 +0900

    InitializingBean  - 의존관계 주입이 끝나면 호출 DisposableBean  - 스프링 종료전 호출
    이 인터페이스는 스프링 전용 인터페이스다. 해당 코드가 스프링 전용 인터페이스에 의존한다. 초기화, 소멸 메서드의 이름을 변경할 수 없다.
    내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없다.
    인터페이스를 사용하는 초기화, 종료 방법은 스프링 초창기에 나온 방법들이고, 지금은 다음의 더 나은 방법들이 있어서 거의 사용하지 않는다.

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +27,20 @@ public class NetworkClient {
     //서비스종료시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+    // 의존관계 주입이 끝나면 호출
+    // 스프링에 의존성이 심해지므로 요즘에는 거의 사용하지 않는다.
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
