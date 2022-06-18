3248ff510af78305dd41d511cc39f2dfb3002ead
SungJaeCho <liadus.veil@gmail.com>
2022-06-19 06:34:44 +0900

    Bean등록 초기화 종료 메서드 @Bean(initMethod = "메서드명", destroyMethod = "메서드명") - 스프링 의존성없음

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -30,17 +27,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     // 의존관계 주입이 끝나면 호출
-    // 스프링에 의존성이 심해지므로 요즘에는 거의 사용하지 않는다.
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
